5a74737f3e71d5cd1a77c13289eb995d49b6fc63
anirudh786 <anirudhjain_sanjay@srmuniv.edu.in>
2018-07-24 21:15:27 +0530

    blood donation

---
+++ admin/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="nav navbar-fixed-bottom">
+            <h3>Made By: <strong>ANIRUDH JAIN</strong> -FROM SRM UNIVERSITY </h3>
+            <br>
+            &COPY;projectworlds
+</footer>
+
+
+
+</body>
+</html>
---
+++ layout/_footer.php
@@ -0,0 +1,9 @@
+<footer class="foter">
+            <b>Made By: <a href="http://projectworlds.in"><strong>Anirudh Jain</strong></a></b>
+            
+</footer>
+
+
+
+</body>
+</html>
